40cb41dca66826c2050b41a01d416dbf9455c828
Aleksandr Sedelnikov <sedelnikov1802@gmail.com>
2018-12-06 17:16:58 +0500

    VRTK prefab, catchanimal cs

--- Assets/Poly/Scripts/test/CatchAnimal.cs
+++ Assets/Poly/Scripts/test/CatchAnimal.cs
@@ -8,7 +8,9 @@ public class CatchAnimal : MonoBehaviour {
 	public Transform neck;
 	public LayerMask animalMask;
 	[Header("UI")]
+	public RectTransform catchTextTrans;
 	public Image fillImage;
+	public Text adviseText;
 	public Text mouseText,
 	rabbitText, chickenText;
 
@@ -17,20 +19,28 @@ public class CatchAnimal : MonoBehaviour {
 	RaycastHit hit;
 
 	void Update () {
+		adviseText.enabled = false;
 		Ray ray = new Ray (neck.position, neck.forward);
-		if(Physics.SphereCast (ray, 2, out hit, 3, animalMask) && Input.GetMouseButton(0))
-			Catch ();
+		if (Physics.SphereCast (ray, 2, out hit, 3, animalMask)) {
+			adviseText.enabled = true;
+			if (Input.GetMouseButton (0))
+				Catch ();
+			else
+				ResetFill ();
+		}
 		else
 			ResetFill ();
 	}
 
 	void Catch () {
+		adviseText.enabled = false;
 		catchTimer += Time.deltaTime;
 		fillImage.fillAmount = catchTimer / 5;
 		if (fillImage.fillAmount == 1) {
 			hit.collider.gameObject.SetActive (false);
-			UpdateUI ();
+			UpdateStatUI ();
 			ResetFill ();
+			StartCoroutine ("MoveCatchText");
 		}
 	}
 
@@ -39,7 +49,7 @@ public class CatchAnimal : MonoBehaviour {
 		catchTimer = 0;
 	}
 
-	void UpdateUI () {
+	void UpdateStatUI () {
 		if (hit.collider.CompareTag ("Mouse")) {
 			catchedM++;
 			mouseText.text = catchedM.ToString();
@@ -52,5 +62,25 @@ public class CatchAnimal : MonoBehaviour {
 			catchedC++;
 			chickenText.text = catchedC.ToString();
 		}
+	}
+
+	IEnumerator MoveCatchText () {
+		catchTextTrans.anchoredPosition = Vector3.zero;
+		Vector3 movement = new Vector3 (1, 1, 0);
+		float timer = 0;
+		Text text = catchTextTrans.GetComponent<Text> ();
+		Color alpha = text.color;
+		alpha.a = 1;
+		catchTextTrans.gameObject.SetActive (true);
+		while (true) {
+			timer += Time.deltaTime;
+			alpha.a -= Time.deltaTime * 0.5f;
+			text.color = alpha;
+			catchTextTrans.Translate (movement);
+			if (timer > 2)
+				break;
+			yield return null;
+		}
+		catchTextTrans.gameObject.SetActive (false);
 	}
 }
