8228cc24f7d57be9f7f267a5d1f0489fa39625e5
Galimsky Artur <motorokz@gmail.com>
2018-12-06 20:20:38 +0500

    Работа со звуком, звуки дыхания, перемещения, звуки животных

---
+++ Assets/Poly/Scripts/AudioControl/AnimalAudioController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalAudioController : MonoBehaviour {
+    [SerializeField] AudioSource voiceSource;
+    [SerializeField] AudioClip voiceClip;
+
+
+	// Use this for initialization
+	void Start () {
+        voiceSource.clip = voiceClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!voiceSource.isPlaying)
+            voiceSource.Play();
+		
+	}
+}
---
+++ Assets/Poly/Scripts/AudioControl/PlayerAudioController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudioController : MonoBehaviour {
+
+    [SerializeField] AudioSource legSource;
+    [SerializeField] AudioSource breathSource;
+
+    [SerializeField]AudioClip runClip;
+    [SerializeField] AudioClip breathClip;
+
+    
+	// Use this for initialization
+	void Start () {
+        legSource.clip = runClip;
+        breathSource.clip = breathClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if(!legSource.isPlaying)
+                legSource.Play();
+            if (!breathSource.isPlaying)
+                breathSource.Play();
+        }
+        else
+        {
+            if(legSource.isPlaying)
+                legSource.Stop();
+            if (breathSource.isPlaying)
+                breathSource.Play();
+        }
+    }
+}
