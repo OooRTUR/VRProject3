4829ea04aa3422ed484c9973e8a017ddb72bddf5
urimpich <motorokz@gmail.com>
2018-12-12 16:05:01 +0300

    Добавлено GameMenu. В нем отображается статистика пойманых животных

---
+++ Assets/Poly/Scripts/GameMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameMenu : MonoBehaviour {
+
+    public static GameMenu instance;
+
+    public int mouses, rabbits, chickens;
+    public Text mT, rT, cT;
+
+    public Transform menuPoint;
+    Canvas canvas;
+    RectTransform rectTrans;
+
+    private void Awake()
+    {
+        instance = this;
+        canvas = GetComponent<Canvas>();
+        rectTrans = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        UpdateMenu();
+    }
+
+    private void Update()
+    {
+        if(canvas.enabled) {
+            float distance = Vec3Mathf.DistanceTo(rectTrans.position, menuPoint.position);
+            if (distance > 20)
+                canvas.enabled = false;
+        }
+        if(OVRInput.Get(OVRInput.Button.One)) {
+            rectTrans.position = menuPoint.position;
+            rectTrans.rotation = Quaternion.LookRotation(menuPoint.forward);
+            canvas.enabled = true;
+        }
+    }
+
+    public void UpdateMenu ()
+    {
+        mT.text = "Мыши: " + mouses.ToString();
+        rT.text = "Зайцы: " + rabbits.ToString();
+        cT.text = "Куропатки: " + chickens.ToString();
+    }
+}
