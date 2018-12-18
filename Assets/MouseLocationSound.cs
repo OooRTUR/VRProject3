5ce07b42c269c5a4b4404fc914846bac4108d6eb
Galimsky Artur <motorokz@gmail.com>
2018-12-18 19:34:21 +0500

    sounds доработан

--- Assets/MouseLocationSound.cs
+++ Assets/MouseLocationSound.cs
@@ -6,8 +6,16 @@ public class MouseLocationSound : MonoBehaviour {
     public Transform rabbit;
     public delegate void MethodHandler(GameObject obj);
     public event MethodHandler OnEnter;
-
+    Renderer rend;
     bool isTriggered = false;
+
+
+    private void Start()
+    {
+        //mat = GetComponent<Renderer>().material;
+        rend = GetComponent<Renderer>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (!isTriggered)
@@ -21,6 +29,19 @@ public class MouseLocationSound : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		
+        float playerRot = 360.0f - Player.instance.transform.rotation.eulerAngles.y;
+        float rotToTarget = Vec3Mathf.GetAngle1(Player.instance.transform.position, transform.position);
+        float deltaRot = playerRot - rotToTarget;
+
+        if(Mathf.Abs(deltaRot) < 30.0f)
+        {
+            rend.enabled = true;
+        }
+        else
+        {
+            rend.enabled = false;
+        }
+
+        Debug.Log(playerRot + " | " + rotToTarget + " | " + deltaRot);
 	}
 }
---
+++ Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public static Player instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(instance);
+    }
+}
